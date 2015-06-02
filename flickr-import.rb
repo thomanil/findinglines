90c1b8b37e4cfb00af1407bbd0a733a0075c7173
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2015-06-02 23:40:00 +0200

    Add all the code I need for my api ops

--- flickr-import.rb
+++ flickr-import.rb
@@ -1,3 +1,49 @@
 require 'flickr'
-flickr = Flickr.new('some_flickr_api_key')    # create a flickr client (get an API key from http://api.flickr.com/services/api/)
-user = flickr.users('sco@scottraymond.net')
+
+# Have to monkeypatch "taken" accessor myself, for some reason not
+# implemented (properly?) by flickr gem
+class Flickr
+  class Photo
+    def taken
+      info = @client.photos_getInfo('photo_id'=>@id)['photo']
+      info['dates']['taken']
+    end
+    def description
+      info = @client.photos_getInfo('photo_id'=>@id)['photo']
+      content = info['description']
+      if content.is_a? String
+        return content
+      else
+        return ""
+      end
+    end
+  end
+end
+
+# Api connect
+api_key = File.open("flickr-api-key").read.strip
+flickr = Flickr.new(api_key)
+user = flickr.users('tegnethomas@yahoo.no')
+
+# Handle photos
+user.photos.each do |photo|
+  title = photo.title
+  description = photo.description
+  taken = photo.taken
+
+  puts title
+  puts description
+  puts taken
+
+  #Save file to disk
+  #File.open(photo.filename, 'w') do |file|
+  #  file.puts photo.file
+  #end
+
+  puts "----"
+end
+
+
+
+#require 'pry'
+#binding.pry
